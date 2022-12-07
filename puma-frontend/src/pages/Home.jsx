d889b9e552686bb338d0f2fbb41b377230640fbd
Sibora Baboci <borababoci1@gmail.com>
2022-12-07 01:55:59 +0100

    UI fixxes

--- puma-frontend/src/pages/Home.jsx
+++ puma-frontend/src/pages/Home.jsx
@@ -6,17 +6,20 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHome } from '@fortawesome/free-solid-svg-icons'
 import Footer from '../components/Footer'
 import { useNavigate } from 'react-router'
+// import imgPath from '../images/homepage.jpg'
 
 const Home = () => {
   const dispatch = useDispatch()
   const navigate = useNavigate()
+
+  const { user: currentUser } = useSelector((state) => state.auth)
 
   useEffect(() => {
     // dispatch()
   }, [])
 
   const getStarted = () => {
-    navigate('/register')
+    navigate('/login')
   }
 
   return (
@@ -25,12 +28,32 @@ const Home = () => {
         className="Jumbotron-landing d-flex align-items-center min-vh-100"
         style={{ fontFamily: 'Lato' }}
       >
-        <Container className="text-center">
-          <h1 style={{ fontSize: '4.2em' }}>Test your strategy</h1>
-          <p style={{ fontSize: '0.8em' }}>Find out how well your bot can trade within minutes!</p>
-          <Button variant="success" size="lg" onClick={getStarted}>
-            <h1>Get Started</h1>
-          </Button>
+        <Container className="">
+          <Row>
+            <Col sm={6} className="text-center">
+              <h1 style={{ fontSize: '4.2em', paddingTop: '60px' }}>Test your strategy</h1>
+              <p style={{ fontSize: '1.2em' }}>Find out how well your bot can trade within minutes!</p>
+              {
+                currentUser ? (
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                ) : (
+                <Button variant="success" size="lg" onClick={getStarted}>
+                  <h1>Get Started</h1>
+                </Button>
+                )
+              }
+              
+            </Col>
+            <Col sm={6}>
+              <img src='/images/homepage.jpg' style={{
+                maxHeight: '100%',
+                maxWidth: '100%',
+                backgroundSize: 'cover'
+              }}/>
+            </Col>
+          </Row>
         </Container>
       </div>
     </main>
