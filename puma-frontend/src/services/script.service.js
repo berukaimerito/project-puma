f70746d92e5f95c233d30f73d44be2ef447b042f
Sibora Baboci <borababoci1@gmail.com>
2022-12-18 02:53:08 +0100

    scripts changed to real api requests

--- puma-frontend/src/services/script.service.js
+++ puma-frontend/src/services/script.service.js
@@ -1,62 +1,31 @@
 import authHeader from "./authHeader";
+import axios from "axios";
 
-// MOCK DATA
-let scripts = [
-  {
-    _id: "1",
-    currency: "BTC",
-    path: "images/btc.jpeg",
-    code: `/**
-    * Default code.
-    */
-    
-    
-    const basicFunction = () => {
-     return 20+20;
-    };
-    
-    const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    const target = 5;
-    console.log(basicFunction());
-    `,
-  },
-  {
-    _id: "2",
-    currency: "ETH",
-    path: "images/eth.jpeg",
-    code: `/**
-    * Default code.
-    */
-    
-    
-    const basicFunction = () => {
-     return 1+1;
-    };
-    
-    const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    const target = 5;
-    console.log(basicFunction());
-    `,
-  },
-];
+const API_URL = "http://localhost:5000/"; // backend api
 
-const API_URL = "localhost:5000/"; // backend api
-
-const saveScript = (username, email, password) => {};
+const saveScript = async (username, script, symbol, interval) => {
+  return axios
+    .post(
+      API_URL + "scripts",
+      { username, script, symbol, interval },
+      { headers: authHeader() }
+    )
+    .then((response) => {
+      console.log(response.data);
+      return response.data;
+    });
+};
 
 const getScriptById = (symbol) => {
   return axios.get(API_URL + "scripts/" + symbol, { headers: authHeader() });
 };
 
-const deleteById = (id) => {
-  let scriptObjs;
-  scriptObjs = scripts.filter((script) => script._id !== id);
-
-  return scriptObjs;
+const deleteById = (symbol) => {
+  return axios.delete(API_URL + "scripts/" + symbol);
 };
 
 const getAllScripts = (id) => {
-  return scripts;
+  return axios.get(API_URL + "scripts", { headers: authHeader() });
 };
 
 const scriptService = {
