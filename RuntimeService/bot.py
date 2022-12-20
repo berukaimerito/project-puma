9861279c03fcef41dd713e0b7ab6f1e622e87e58
berukaimerito <kocakberkaymert@gmail.com>
2022-12-20 19:16:24 +0100

    beta bot class

--- RuntimeService/bot.py
+++ RuntimeService/bot.py
@@ -1,39 +1,65 @@
-from dotenv import dotenv_values
+from abstractbot import Abc
+import requests
+import json
 
 config = dotenv_values()
-from abstractbot import Abc
-import json
 
 
 class Bot(Abc):
     timestamp = ''
 
-
-
-
     def __init__(self, username, symbol, app):
         self.username = username
         self.symbol = symbol
         self.app = app
+        self.data = None
+        self.open_price = None
+        self.close_price = None
+        self.open_timestamp = None
+        self.close_timestamp = None
+        self.on_going = False
+        self.profit = False
+        self.finished = False
+
+    def sell(self, ts, price):
+        self.on_going = False
+        self.close_timestamp = str(ts)
+        self.close_price = float(price)
+        print('sell ici')
+
+    def buy(self, ts, price):
+        self.on_going = True
+        self.open_timestamp = str(ts)
+        self.open_price = float(price)
+
+        print('buy ici')
+
+    def on_price_change(self, data, ts, price):
+        print((price))
+        if price > 0.4 and not self.on_going:
+            self.buy(ts, price)
+        if self.on_going and price > 0.4:
+            self.sell(ts, price)
+            self.calculate_closed_script()
+            # self.close_channels()
+    # def calculate_current_percentage(self):
+    #     self.profit
+
+    def calculate_closed_script(self):
+        self.profit = ((self.open_price - self.close_price) / self.close_price) * 100
+        requests.post('http://127.0.0.1:5000/portfoliotracker',
+                      json={'username': self.username, 'symbol': self.symbol, 'profit': self.profit})
 
 
 
 
 
-    @staticmethod
-    def byte_to_dictionary(str):
-        res = json.loads(str)
-        res['CandleStick'] = json.loads(res['CandleStick'])
-        res['Ticker'] = json.loads(res['Ticker'])
-        return res
-
-    # def buy(self):
+  
+    
 
 
 
-    # def on_price_change(self,data):
-    #     if data['CandleStick']['high']==30:
-    #         buy()
+  
 
 
 
